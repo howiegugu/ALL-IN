ee24e6d79f22892983f1657c6deb2ccbcbe62fdb
chenzhihao <1418959754@qq.com>
2023-05-13 22:59:56 +0800

    fix:调整单元测试

--- src/test/java/com/guguhowie/testing/EmployeeRestControllerIntegrationTest.java
+++ src/test/java/com/guguhowie/testing/EmployeeRestControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -34,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @EnableAutoConfiguration(exclude=SecurityAutoConfiguration.class)
 @TestPropertySource(locations = "classpath:application-integrationtest.properties")
 @AutoConfigureTestDatabase
+@EnableJpaRepositories
 public class EmployeeRestControllerIntegrationTest {
 
     @Autowired
